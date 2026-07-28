2e6629cdd96ecb17f3d6e9560a44f47854f91b61
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 00:44:33 +0300

    Use .map method to loop over pets instead of hardcoding each pet card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import PetCard from "../components/PetCard";
 
 function Home() {
+    const pets = [
+  {id: 1, name: "Max", breed: "Golden Retriever", age: 2, },
+  {id: 2,name: "Luna ", breed: "Persian Cat", age: 1,},
+  {id: 3, name: "Buddy ", breed: "Beagle", age: 3, },
+];
   return (
     <div>
       <h1>Welcome to PawFound 🐾</h1>
       <p>Find your perfect pet companion.</p>
 
-      <PetCard name="Max" breed="Golden Retriever" age={2}/>
-      <PetCard name="Luna" breed="Persian Cat" age={1} />
-      <PetCard name="Buddy" breed="Beagle" age={3} />
+      {pets.map((pet) => (
+   <PetCard key={pet.id} name={pet.name} breed={pet.breed} age={pet.age} />
+     ))}
+ 
     </div>
   );
 }
