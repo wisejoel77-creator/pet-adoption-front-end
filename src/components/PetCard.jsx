df83b9ff578e3559782ebf65d29a76918a481a70
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 00:28:08 +0300

    Add code to my PetCard.jsx file that shows pet details

---
+++ src/components/PetCard.jsx
@@ -0,0 +1,19 @@
+function PetCard() {
+  return (
+    <div
+      style={{
+        border: "1px solid gray",
+        borderRadius: "10px",
+        padding: "20px",
+        width: "250px",
+      }}>
+     <h2>{props.name}</h2>
+      <p>{props.breed}</p>
+      <p>Age: {props.age} years</p>
+     
+      <button>View Details</button>
+    </div>
+  );
+}
+
+export default PetCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
+import PetCard from "../components/PetCard";
+
 function Home() {
   return (
     <div>
-      <h1>Welcome to PawFound </h1>
+      <h1>Welcome to PawFound 🐾</h1>
       <p>Find your perfect pet companion.</p>
+
+      <PetCard name="Max" breed="Golden Retriever" age={2}/>
+      <PetCard name="Luna" breed="Persian Cat" age={1} />
+      <PetCard name="Buddy" breed="Beagle" age={3} />
     </div>
   );
 }
