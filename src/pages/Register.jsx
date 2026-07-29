adb64b48e89117da62189da389ad114040262480
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 12:05:29 +0300

    Set state for formData

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,37 +1,44 @@
 import { useState } from "react";
 function Register() {
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  
   const [error, setError] = useState("");
-
+  const [formData, setFormData] = useState({
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: ""
+});
+  function handleChange(event) {
+  setFormData({
+    ...formData,
+    [event.target.name]: event.target.value
+  });
+}
   function handleRegister(event) {
     event.preventDefault();
 
-    if (name === "" || email === "" ||
-      password === "" || confirmPassword === ""
+    if (formData.name === "" || formData.email === "" ||
+      formData.password === "" || formData.confirmPassword === ""
     ) {
       setError("Please fill in all fields.");
       return;
     }
 
-    if (password.length < 8) {
+    if (formData.password.length < 8) {
       setError("Password must be at least 8 characters.");
       return;
     }
 
-    if (password !== confirmPassword) {
+    if (formData.password !== formData.confirmPassword) {
       setError("Passwords do not match.");
       return;
     }
 
     setError("");
 
-    console.log("Name:", name);
-    console.log("Email:", email);
-    console.log("Password:", password);
+    console.log("Name:", formData.name);
+    console.log("Email:", formData.email);
+    console.log("Password:", formData.password);
 
     alert("Account created successfully!");
   }
@@ -42,15 +49,15 @@ function Register() {
 
       <form onSubmit={handleRegister}>
         <input type="text" placeholder="Enter your name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}/>
+          name="name" value ={formData.name}
+          onChange={handleChange}/>
 
         <br />
         <br />
 
         <input type="email" placeholder="Enter your email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          name="email" value ={formData.email}
+          onChange={handleChange}
         />
 
         <br />
@@ -58,16 +65,16 @@ function Register() {
 
         <input
           type="password" placeholder="Enter your password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)} />
+          name="password" value ={formData.password}
+          onChange={handleChange} />
 
         <br />
         <br />
 
         <input
           type="password" placeholder="Confirm your password"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)} />
+          name="confirmPassword" value ={formData.confirmPassword}
+          onChange={handleChange} />
 
         <br />
         <br />
