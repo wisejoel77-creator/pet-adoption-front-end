832c3652cc356fc7d490c189c48ccd6262f98d2e
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 10:56:18 +0300

    Add a form tag in my login.jsx file

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,28 +4,36 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   
-  function handleLogin() {
+  function handleLogin(event) {
+  event.preventDefault();
+  
   console.log(email);
   console.log(password);
 }
   return (
     <div>
-      <h1>Login</h1>
+  <h1>Login</h1>
 
-      <input type="email" placeholder="Enter your email" value={email}
-        onChange={(e) => setEmail(e.target.value)} />
+  <form onSubmit={handleLogin}>
+    <input type="email" placeholder="Enter your email" value={email}
+      onChange={(e) => setEmail(e.target.value)} />
 
-      <br />
-      <br />
+    <br />
+    <br />
 
-      <input type="password" placeholder="Enter your password" value={password}
-        onChange={(e) => setPassword(e.target.value)}/>
+    <input type="password" placeholder="Enter your password" value={password}
+      onChange={(e) => setPassword(e.target.value)}
+    />
 
-      <br />
-      <br />
+    <br />
+    <br />
 
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <button type="submit"> Login</button>
+  </form>
+
+  <p>Email: {email}</p>
+  <p>Password: {password}</p>
+</div>
   );
 }
 
