cc83c1cc230c007f297452068c8209a74b3e9b57
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 10:24:36 +0300

    Add search bar feature and a button that counts how  many pets have been viewed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import PetCard from "../components/PetCard";
+import { useState } from "react";
 
 function Home() {
     const pets = [
@@ -6,14 +7,23 @@ function Home() {
   {id: 2,name: "Luna ", breed: "Persian Cat", age: 1,},
   {id: 3, name: "Buddy ", breed: "Beagle", age: 3, },
 ];
-  return (
+    const [count, setCount] = useState(0);
+    const [search, setSearch] = useState("");
+ 
+   return (
     <div>
+
       <h1>Welcome to PawFound 🐾</h1>
       <p>Find your perfect pet companion.</p>
-
-      {pets.map((pet) => (
-   <PetCard key={pet.id} name={pet.name} breed={pet.breed} age={pet.age} />
-     ))}
+      <h2>Pets Viewed: {count}</h2>
+      <button onClick={() => setCount(count + 1)}>View Another Pet</button>
+      <input type="text" placeholder="Search pets..." value={search}
+       onChange={(event) => setSearch(event.target.value)}/>
+      
+     {pets.filter((pet) =>
+    pet.name.toLowerCase().includes(search.toLowerCase()) )
+    .map((pet) => (<PetCard key={pet.id} name={pet.name} breed={pet.breed} age={pet.age}/>
+  ))}
  
     </div>
   );
