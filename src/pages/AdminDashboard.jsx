7a49b453dc8b00d48ba7ff77a48b5850effd3331
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 18:06:54 +0300

    Create ManageShelters.jsx and AdminRequests.jsx files

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -3,30 +3,34 @@ import { Link } from "react-router-dom";
 function AdminDashboard() {
   return (
     <div className="admin-dashboard">
-      <h1>Admin Dashboard 🐾</h1>
+      <h1> Admin Dashboard</h1>
       <p>Welcome, Administrator.</p>
 
       <div className="dashboard-links">
 
         <div className="card">
           <h3> Manage Pets</h3>
-          <p>Add, edit and remove pets.</p>
+          <p>Add, edit, and remove pets.</p>
 
           <Link to="/admin/pets"> <button>Manage Pets</button> </Link>
         </div>
 
         <div className="card">
           <h3> Adoption Requests</h3>
-          <p>Approve or reject requests.</p>
-
-          <Link to="/admin/requests"> <button>View Requests</button></Link>
+          <p>Approve or reject adoption requests.</p>
+          <Link to="/admin/requests"> <button>View Requests</button> </Link>
         </div>
 
         <div className="card">
-          <h3> Users</h3>
-          <p>View registered users.</p>
+          <h3> Shelters</h3>
+          <p>Create, edit, remove and manage shelters.</p>
+          <Link to="/admin/shelters"> <button>Manage Shelters</button> </Link>
+        </div>
 
-          <Link to="/admin/users"> <button>View Users</button></Link>
+        <div className="card">
+          <h3> Dashboard Statistics</h3>
+          <p>View system statistics and reports.</p>
+          <Link to="/admin/statistics"> <button>View Statistics</button></Link>
         </div>
 
       </div>
