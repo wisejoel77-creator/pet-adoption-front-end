462e2da35419624c7ac61c07d09509f08cac0ffd
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 12:31:21 +0300

    Implement useEffect in home.jsx file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,38 @@
 import PetCard from "../components/PetCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Home() {
-    const pets = [
-  {id: 1, name: "Max", breed: "Golden Retriever", age: 2, },
-  {id: 2,name: "Luna ", breed: "Persian Cat", age: 1,},
-  {id: 3, name: "Buddy ", breed: "Beagle", age: 3, },
-];
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    const [pets, setPets] = useState([]);
     const [count, setCount] = useState(0);
     const [search, setSearch] = useState("");
+    useEffect(() => {
+
+  const fetchPets = async () => {
+    try {
+
+      const response = await fetch( "https://jsonplaceholder.typicode.com/users" );
+      const data = await response.json();
+
+      const formattedPets = data.map((user) => ({
+        id: user.id, name: user.name,
+        breed: "Unknown breed", age: 1
+      }));
+
+      setPets(formattedPets);
+
+    } catch (error) {
+      setError("Failed to load pets.");
+    } finally {
+      setLoading(false);
+    }
+
+  };
+
+  fetchPets();
+
+}, []);
  
    return (
     <div>
@@ -20,6 +44,7 @@ function Home() {
       <input type="text" placeholder="Search pets..." value={search}
        onChange={(event) => setSearch(event.target.value)}/>
       
+      {loading && <p>Loading pets...</p>}
       {pets.filter((pet) =>
     pet.name.toLowerCase().includes(search.toLowerCase()))
   .map((pet) => (
