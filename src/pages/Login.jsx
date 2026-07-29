e76c9c8d55d7778de254d81f6dde53813d6b329c
wisejoel77-creator <wisejoel77@gmail.com>
2026-07-29 11:19:50 +0300

    Add validations and show error messages if password or email fields are blank

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,17 +3,29 @@ import { useState } from "react";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   
   function handleLogin(event) {
   event.preventDefault();
+  if (email === "" || password === "") {
+    setError("Please fill in all fields.");
+    return;
+  }
+
+  if (password.length < 8) {
+    setError("Password must be at least 8 characters.");
+    return;
+  }
+  setError("");
   
   console.log(email);
   console.log(password);
+  alert("Login successful!");
 }
   return (
     <div>
   <h1>Login</h1>
-
+  {error && <p style={{ color: "red" }}>{error}</p>}
   <form onSubmit={handleLogin}>
     <input type="email" placeholder="Enter your email" value={email}
       onChange={(e) => setEmail(e.target.value)} />
